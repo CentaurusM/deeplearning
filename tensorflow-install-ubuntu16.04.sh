924f2d4a83886caf761fba4224a561c6ddd252e4
CentaurusM <inboxmotor@gmail.com>
2018-03-30 10:17:29 +0800

    Update tensorflow-install-ubuntu16.04.sh

--- tensorflow-install-ubuntu16.04.sh
+++ tensorflow-install-ubuntu16.04.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
- 
+
 # install cuda 9.0
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 apt-get update
 apt-get install cuda-9-0 -y
 
-# config enviroment for cuda
-echo "export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+# set enviroment for cuda
+echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-" > env_cuda.rc
+' > env_cuda.rc
 source env_cuda.rc
 
 # install cudnn 7.0
@@ -26,7 +26,7 @@ pip install tensorflow-gpu==1.6.0 -i https://pypi.douban.com/simple
 
 # tensorflow benchmark
 apt install unzip -y
-# wget https://github.com/tensorflow/benchmarks/archive/tf_benchmark_stage.zip 
+wget https://github.com/tensorflow/benchmarks/archive/tf_benchmark_stage.zip 
 unzip tf_benchmark_stage.zip
 cd ./benchmarks-tf_benchmark_stage/scripts/tf_cnn_enchmarks  
 #python tf_cnn_benchmarks.py
